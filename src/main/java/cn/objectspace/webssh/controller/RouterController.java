255c803fff2e0d97a3cd49776df1894e1dfce827
xiejiajun <xiejiajun02@163.con>
2021-01-29 14:32:05 +0800

    个人测试

--- src/main/java/cn/objectspace/webssh/controller/RouterController.java
+++ src/main/java/cn/objectspace/webssh/controller/RouterController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RouterController {
-    @RequestMapping("/websshpage")
+    @RequestMapping("/")
     public String websshpage(){
         return "webssh";
     }
